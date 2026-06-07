a65af494e5bf9cc799a6b9f2f87c4efd2911794a
Sharmadotcom <sharmasarvesh390@gmail.com>
2026-06-07 16:30:12 +0530

    chore: clean repository and remove sensitive files

--- test4.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(r"C:\Study\Projects\password-manager\database.db")
-cursor = conn.cursor()
-cursor.execute("DELETE FROM vault")
-conn.commit()
-conn.close()
-print("Done")
